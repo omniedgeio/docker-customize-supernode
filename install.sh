2f981cab4e5704f293eaaa529e37aea797073e3b
yongqian <bruceboty@gmail.com>
2022-06-09 16:42:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ sudo apt-get update && sudo apt-get install docker.io -y
 git clone https://github.com/omniedgeio/docker-customize-supernode.git
 cd docker-customize-supernode
 sudo docker build -t docker-customize-supernode .
-docker run -d -p 443:443/udp docker-customize-supernode
+sudo docker run -d -p 443:443/udp docker-customize-supernode
 sudo ufw allow 443
