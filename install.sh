17a06db115507244ec425c62bebacc15d26feaaf
Yong <hi@omniedge.io>
2022-06-09 16:19:23 +0800

    update():change default port to 443, add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash bash
+
+set -e
+
+sudo apt-get update && sudo apt-get install docker.io -y
+git clone https://github.com/omniedgeio/docker-customize-supernode.git
+cd docker-customize-supernode
+sudo docker build -t docker-customize-supernode .
+docker run -d -p 443:443/udp docker-customize-supernode
+sudo ufw allow 443
